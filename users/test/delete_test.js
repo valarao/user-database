06a2a6d467b61c1b9998fde6dd7604b0655142f7
u0e1b <miguel.valarao@gmail.com>
2019-05-14 21:31:16 -0700

    Tests added

---
+++ users/test/delete_test.js
@@ -0,0 +1,42 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Deleting a user', () => {
+    let joe;
+
+    beforeEach((done) => {
+        joe = new User({ name: 'Joe' });
+        joe.save()
+            .then(() => done());
+    });
+
+    it('model instance remove', (done) => {
+        joe.remove()
+            .then(() => User.findOne({ name: 'Joe'}))
+            .then((user) => {
+                assert(user === null);
+                done();
+            });
+    });
+
+    it('class method deleteMany', (done) => {
+        // Remove a bunch of records with some given criteria
+        User.deleteMany({ name: 'Joe' })
+        .then(() => User.findOne({ name: 'Joe'}))
+            .then((user) => {
+                assert(user === null);
+                done();
+            });
+    });
+
+    it('class method deleteOne', (done) => {
+        // Remove a single record with some given criteria
+        User.deleteOne({ name: 'Joe' })
+        .then(() => User.findOne({ name: 'Joe'}))
+            .then((user) => {
+                assert(user === null);
+                done();
+            });
+    });
+
+});
